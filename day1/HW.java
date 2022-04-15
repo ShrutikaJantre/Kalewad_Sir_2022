a68a54c4f4e4d0362fed5c82951d2aaf469203d5
ShrutikaJantre <81174919+ShrutikaJantre@users.noreply.github.com>
2022-04-15 19:32:28 +0530

    Add files via upload

---
+++ day1/HW.java
@@ -0,0 +1,6 @@
+class HelloWorld{
+	System.out.println("Hello World");
+	byte b = 130;
+	byte b = (byte) 130;
+	System.out.printlb(b);
+}
---
+++ day1/MathOperDemo.java
@@ -0,0 +1,12 @@
+class MathOperations{
+	public static void main(String[] args){
+		System.out.println("1");
+
+}
+
+class MathOperDemo{
+	public static void main(String[] args){
+		System.out.println("2");
+	
+	}
+}
